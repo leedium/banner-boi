329f9be89df647c23864d4f0427c56e61e324b03
fumingxia <david@leedium.com>
2015-09-05 06:10:51 +0200

    -cleaned unused files - updated preview page

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 #Banner Boi   :)
-Automated HTML5 Standard Banner Creation Tool to save you time.  *
+EASY TO USE Automated HTML5 Standard Banner Creation Tool to save you crazy amounts of time.
 
 ##Includes
-Multi Provider support for DoubleClick, Sizmek, Generic Publisher, customizable to add more providers
+Multi Provider support for DoubleClick, Sizmek, ... more to come!
 8 of the most popular sizes.
-300x250, 336x280, 250x250, 120x600, 160x600, 300x600, 120x240, 728x90
-SASS, css-pleeease, pre post css compiler
+[300x250, 336x280, 250x250, 120x600, 160x600, 300x600, 120x240, 728x90]
+GSAP - Greensock Timeline / TweenLite CDN for animations.
+SASS, css-pleeease, pre post css compiler.
 Full Support for IE9 and above.
 Handlebars for templating.
-html minification
-image optimization
-ability to override master version.
-browser-sync
+Ability to override master version.
+Browser-Sync
 
+Coming SOON! - image optimization / sprite sheet generation
 
 
 ## Getting Started
--- bannerConfig.js
+++ bannerConfig.js
@@ -2,33 +2,17 @@ var bannerConfig = {
     dest: 'delivery',
     build : "DEV",
     campaignName : "leedium",
-    providers : ["dck","sizmek"],
+    providers : [
+        {
+            id:"dck",
+            headerScript:'<script src="https://s0.2mdn.net/ads/studio/Enabler.js"></script>',
+            footerScript:'<script type="text/javascript"> window.onload = function(){if (Enabler.isInitialized()){enablerInitHandler(); }else{ Enabler.addEventListener(studio.events.StudioEvent.INIT,  enablerInitHandler);}}function enablerInitHandler() { startBanner(); } </script>'
+        },
+        {
+            id:"sizmek",
+            headerScript:'<script type="text/javascript" src="http://ds.serving-sys.com/BurstingScript/EBLoader.js"></script>',
+            footerScript:'<script type="text/javascript">function checkInit(){EB.isInitialized()?onInit():EB.addEventListener(EBG.EventName.EB_INITIALIZED,onInit)}function onInit(){startBanner()}</script><script type="text/javascript">checkInit()</script>'
+        }],
     sizes : ["120x600","120x240","160x600","300x250","250x250","300x600","336x280","728x90"]
 }
 module.exports = bannerConfig;
-
-
-
-
-/*providerPaths = {
- dck: [
- PATH_DCK + '/dck_[campaign]_120x600/',
- PATH_DCK + '/dck_[campaign]_120x240/',
- PATH_DCK + '/dck_[campaign]_160x600/',
- PATH_DCK + '/dck_[campaign]_300x250/',
- PATH_DCK + '/dck_[campaign]_250x250/',
- PATH_DCK + '/dck_[campaign]_300x600/',
- PATH_DCK + '/dck_[campaign]_336x280/',
- PATH_DCK + '/dck_[campaign]_728x90/'
- ],
- sizmek: [
- PATH_SIZEMEK + '/sizmek_[campaign]_120x600/',
- PATH_SIZEMEK + '/sizmek_[campaign]_120x240/',
- PATH_SIZEMEK + '/sizmek_[campaign]_160x600/',
- PATH_SIZEMEK + '/sizmek_[campaign]_300x250/',
- PATH_SIZEMEK + '/sizmek_[campaign]_250x250/',
- PATH_SIZEMEK + '/sizmek_[campaign]_300x600/',
- PATH_SIZEMEK + '/sizmek_[campaign]_336x280/',
- PATH_SIZEMEK + '/sizmek_[campaign]_728x90/'
- ]
- }*/
