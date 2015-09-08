331d0bef549612219e34e718bf1c20201c52ff7e
fumingxia <david@leedium.com>
2015-09-08 02:04:08 +0200

    updated-demo

--- bannerConfig.js
+++ bannerConfig.js
@@ -1,19 +1,19 @@
 var bannerConfig = {
     dest: 'dist',
     build : "DEV",
-    campaignName : "leedium",
+    campaignName : "demo",
     providers : [
         {
             id:"dck",
             headerScript:'<script src="https://s0.2mdn.net/ads/studio/Enabler.js"></script>',
-            footerScript:'<script type="text/javascript"> window.onload = function(){if (Enabler.isInitialized()){enablerInitHandler(); }else{ Enabler.addEventListener(studio.events.StudioEvent.INIT,  enablerInitHandler);}}; function enablerInitHandler() { startBanner(); } </script>'
+            footerScript:'<script type="text/javascript"> function enablerInitialized(){Enabler.isVisible()?adVisible():Enabler.addEventListener(studio.events.StudioEvent.VISIBLE,adVisible)};function adVisible(){startBanner()};function bgExitHandler(){Enabler.exit("Main Banner Exit")}Enabler.isInitialized()?enablerInitialized():Enabler.addEventListener(studio.events.StudioEvent.INIT,enablerInitialized);</script>'
         },
         {
             id:"sizmek",
             headerScript:'<script type="text/javascript" src="http://ds.serving-sys.com/BurstingScript/EBLoader.js"></script>',
             footerScript:'<script type="text/javascript">function checkInit(){EB.isInitialized()?onInit():EB.addEventListener(EBG.EventName.EB_INITIALIZED,onInit)};function onInit(){startBanner()}</script><script type="text/javascript">checkInit()</script>'
         }],
-    sizes : ["120x600","120x240","160x600","300x250","250x250","300x600","336x280","728x90"],
+    sizes : ["120x600","120x240","160x600","300x250","250x250","300x600","336x280","728x90","970x250"],
     minifyCSS: false,
     reloadDelay:3000
 }
